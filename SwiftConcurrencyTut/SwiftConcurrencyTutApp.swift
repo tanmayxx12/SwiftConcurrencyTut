dd31a5944f5db942d9f48c81ad787275d23f71b9
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-13 16:45:38 +0530

    Initial Commit

---
+++ SwiftConcurrencyTut/SwiftConcurrencyTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyTutApp.swift
+//  SwiftConcurrencyTut
+//
+//  Created by Tanmay . on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
