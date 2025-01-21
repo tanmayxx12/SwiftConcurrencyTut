4a7eac517aa873dee3d0c87843f8c99844d8b0f2
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-21 15:31:13 +0530

    [Concept] Learned calling asynchronous function using Task in a View

--- SwiftConcurrencyPractice/ConcurrencyPractice/ConcurrencyPractice/CallingAsyncFunc.swift
+++ SwiftConcurrencyPractice/ConcurrencyPractice/ConcurrencyPractice/CallingAsyncFunc.swift
@@ -7,10 +7,50 @@
 
 import SwiftUI
 
+// We call asynchronous functions using Task, .task(), .refreshable() or MainActor.run {}
+
 struct CallingAsyncFunc: View {
+    @State private var site: String = "https://"
+    @State private var sourceCode = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                TextField("Website address...", text: $site)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 45)
+                    .background(.black.opacity(0.2))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 3)
+                
+                Button("Go") {
+                    Task {
+                        await fetchSource()
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.gray)
+                .padding(.trailing, 3)
+            }
+            ScrollView{
+                Text(sourceCode)
+            }
+            
+        }
     }
+    
+    func fetchSource() async {
+        do {
+            let url = URL(string: site)!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            sourceCode = String(decoding: data, as: UTF8.self).trimmingCharacters(in: .whitespacesAndNewlines)
+        } catch {
+            sourceCode = "Failed to fetch: \(site)"
+        }
+    }
+    
 }
 
 #Preview {
