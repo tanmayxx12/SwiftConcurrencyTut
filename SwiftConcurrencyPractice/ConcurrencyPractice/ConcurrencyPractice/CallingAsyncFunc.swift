42ec3f4dcbe074777695e19c53df970444be76f9
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-21 15:30:40 +0530

    [Concept] Calling asynchronous functions using Task in a view

---
+++ SwiftConcurrencyPractice/ConcurrencyPractice/ConcurrencyPractice/CallingAsyncFunc.swift
@@ -0,0 +1,18 @@
+//
+//  CallingAsyncFunc.swift
+//  ConcurrencyPractice
+//
+//  Created by Tanmay . on 21/01/25.
+//
+
+import SwiftUI
+
+struct CallingAsyncFunc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallingAsyncFunc()
+}
